40f3675904ac5a985e4e3ad88e7ea5579edc1989
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-14 22:55:05 +0530

    react router dom added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+          <Route exact path="/"   element={  <HomePage />}/>
+          
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Features from "../components/Features";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
+}
