0cf04103621107883bf6de35205b353f49a65573
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-15 04:06:10 +0530

    dashboard layout added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
 import SideBar from "../components/SideBar";
 import SideSection from "../components/SideSection";
-
+import download from "../download.svg";
 export default function Dashboard() {
-    return (
-        <div className="flex">
-            <SideBar />
-            <SideSection />
+  return (
+    <div className="flex bg-[#37393E] w-full">
+      <SideBar />
+      <SideSection />
+      <div>
+        <div className="flex justify-center items-center w-full h-full">
+          <img src={download} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
